27a7c0143efa8d66f4af391af1bfabe20c4b2278
Yucheng5432 <yuchengsu5256@outlook.com>
2021-11-23 04:09:36 +0800

    11/23/2021

---
+++ data/user.js
@@ -0,0 +1,8 @@
+const mongoCollections = require('../config/mongoCollections');
+const users = mongoCollections.users;
+let {ObjectId} = require('mongodb');
+
+async function insertUsers(){
+    
+}
+
