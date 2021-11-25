ec0cc8e52b4453234225b6a90544a9f694a89a67
rajpaynaik <rajpaynaik18@gmail.com>
2021-11-25 17:12:03 -0500

    adding functions in posts routes

--- routes/posts.js
+++ routes/posts.js
@@ -32,4 +32,32 @@ router.get("/:id", async (req, res) => {
   }
 });
 
+// 3. Get posts by username
+router.get("/userPosts/:username", async (req, res) => {
+  const { username } = req.params.username;
+  if (!username) {
+    return res.status(500).json({ error: "No username parameter!" });
+  }
+  try {
+    const posts = await postFunctions.getUserPosts(username);
+    return res.status(200).json(posts);
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+});
+
+// 4. find post by search term
+router.post("/search/:searchterm", async (req, res) => {
+  const searchTerm = req.params.searchterm;
+  if (!req.params.searchterm) {
+    return res.status(500).json({ error: "No search term provided" });
+  }
+  try {
+    const posts = await postFunctions.findPostsbySearchterm(searchTerm);
+    return res.status(200).json(posts);
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+});
+
 module.exports = router;
