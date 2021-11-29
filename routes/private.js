9ab1518597eded4c80b3aaec4a0e3ddd1df8853d
yw973 <88466332+yw973@users.noreply.github.com>
2021-11-28 23:03:55 -0500

    Add files via upload

---
+++ routes/private.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', async (req, res) => {
+
+    try {
+        res.render('pages/private', { title: "private",username:req.session.user.username });
+      } catch (e) {
+        res.sendStatus(500);
+        }
+      
+    });
+
+module.exports = router;
