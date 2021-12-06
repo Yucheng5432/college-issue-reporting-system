b8339591119dc758c94d73887b19a8ca54c1b2eb
Yash Koladia <ykoladia@stevens.edu>
2021-12-05 19:26:02 -0500

    Edit profile client side form validation done and minor changes in login form client side

--- public/js/login.js
+++ public/js/login.js
@@ -14,38 +14,27 @@
     if (myForm) {
       myForm.addEventListener("submit", (event) => {
         valid = true
-        
 
-        if(!usernameInput.value.trim()){
+      
+        
+        if(usernameInput.value.trim().length == 0){
           event.preventDefault();
           valid = false
-          usernameInput.value = ''
+          // usernameInput.value = ''
           errorDiv.hidden = false
           errorDiv.innerHTML = 'User must enter username' 
           return 
         }
          else{
-           valid = true
+          valid = true
           errorDiv.hidden = true
         }
-
-        if(usernameInput.value.trim().length < 4){
-          event.preventDefault();
-          valid = false
-          usernameInput.value = ''
-          errorDiv.hidden = false
-          errorDiv.innerHTML = 'Length of username must be greater than 4'  
-          return
-        }
-         else{
-           valid = true
-          errorDiv.hidden = true
-        }
+        
 
         if(usernameInput.value.trim().match(/\s/)){
           event.preventDefault();
           valid = false
-          usernameInput.value = ''
+          // usernameInput.value = ''
           errorDiv.hidden = false
           errorDiv.innerHTML = 'Username contains spacess'  
           return
@@ -55,13 +44,27 @@
           errorDiv.hidden = true
         }
 
+
+        if(usernameInput.value.trim().length < 4){
+          event.preventDefault();
+          valid = false
+          // usernameInput.value = ''
+          errorDiv.hidden = false
+          errorDiv.innerHTML = 'Length of username must be greater than 4'  
+          return
+        }
+         else{
+           valid = true
+          errorDiv.hidden = true
+        }
+
+
         
 
-       
         if(!usernameInput.value.trim().match(/^[a-z0-9]+$/i)){
           event.preventDefault();
           valid = false
-          usernameInput.value = ''
+          // usernameInput.value = ''
           errorDiv.hidden = false
           errorDiv.innerHTML = 'Username can only be alphanumeric values'  
           return
@@ -75,7 +78,7 @@
         if(!passswordInput.value.trim()){
           event.preventDefault();
           valid = false
-          passswordInput.value = ''
+          // passswordInput.value = ''
           passErrorDiv.hidden = false
           passErrorDiv.innerHTML = 'User must enter password'
           return
@@ -84,17 +87,31 @@
           passErrorDiv.hidden = true
         }
 
-        // if(!passswordInput.value.length < 6){
-        //   event.preventDefault();
-        //   valid = false
-        //   passswordInput.value = ''
-        //   passErrorDiv.hidden = false
-        //   passErrorDiv.innerHTML = 'Password length is lesser than 6'
-        //   return
-        // }else{
-        //   valid = true
-        //   passErrorDiv.hidden = true
-        // }
+        if(passswordInput.value.trim().match(/\s/)){
+          event.preventDefault();
+          valid = false
+          // usernameInput.value = ''
+          passErrorDiv.hidden = false
+          passErrorDiv.innerHTML = 'Password contains spaces'  
+          return
+        }
+         else{
+           valid = true
+          passErrorDiv.hidden = true
+        }
+
+
+        if(passswordInput.value.trim().length < 6){
+          event.preventDefault();
+          valid = false
+          // passswordInput.value = ''
+          passErrorDiv.hidden = false
+          passErrorDiv.innerHTML = 'Password length is lesser than 6'
+          return
+        }else{
+          valid = true
+          passErrorDiv.hidden = true
+        }
       });
     }
   })();
