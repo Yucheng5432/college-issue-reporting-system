8ab9050ed70acc78274c06a283a61a8541325c4d
rajpaynaik <rajpaynaik18@gmail.com>
2021-12-12 18:25:59 -0500

    adding condition in js file

--- public/js/editPost.js
+++ public/js/editPost.js
@@ -1,23 +1,17 @@
 (function ($) {
-      
-  let imageInput = document.getElementById("error_imgEditpost")
-  let myForm = document.getElementById("editpostForm")
+  let imageInput = document.getElementById("error_imgEditpost");
+  let myForm = document.getElementById("editpostForm");
   if (myForm) {
     myForm.addEventListener("submit", (event) => {
-        // event.preventDefault()
-    // let img = $("#image").val()
-    let image = document.getElementById("image")
-    if ( !(/\.(jpe?g|png|jpg)$/i.test(image.value)) ){
-        event.preventDefault()
-       imageInput.hidden = false
-       imageInput.innerHTML = "please enter a photo/valid photo."
-    }else{
-        imageInput.innerHTML = ""
-        imageInput.hidden = true
-    }
-    
-  });
-}
-  })(window.jQuery);
-  
-   
+      let image = document.getElementById("image");
+      if (image.value && !/\.(jpe?g|png|jpg)$/i.test(image.value)) {
+        event.preventDefault();
+        imageInput.hidden = false;
+        imageInput.innerHTML = "please enter a photo/valid photo.";
+      } else {
+        imageInput.innerHTML = "";
+        imageInput.hidden = true;
+      }
+    });
+  }
+})(window.jQuery);
